7d9da01e12ed87c51bd4a50a8394dfd0ac36a4e8
pyj00 <70153272+pyj00@users.noreply.github.com>
2021-08-16 14:05:25 +0900

    Add files via upload

---
+++ SceneChange.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    public void ChangeTitle()
+    {
+        SceneManager.LoadScene("TitleScene");
+    }
+
+    public void ChangeStory()
+    {
+        SceneManager.LoadScene("StoryScene");
+    }
+
+    public void ChangePlayFive()
+    {
+        SceneManager.LoadScene("5FScene");
+    }
+
+    public void ChangePlayFour()
+    {
+        SceneManager.LoadScene("4FScene");
+    }
+
+    public void ChangePlayThree()
+    {
+        SceneManager.LoadScene("3FScene");
+    }
+
+    public void ChangePlayTwo()
+    {
+        SceneManager.LoadScene("2FScene");
+    }
+    public void ChangePlayOne()
+    {
+        SceneManager.LoadScene("1FScene");
+    }
+}
