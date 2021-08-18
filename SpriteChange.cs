569665fa6422df87dc77c41e5e63792e2e00df46
mkmkkim <74914390+mkmkkim@users.noreply.github.com>
2021-08-18 17:20:13 +0900

    Add files via upload
    4F making

--- SpriteChange.cs
+++ SpriteChange.cs
@@ -14,4 +14,16 @@ public class SpriteChange : MonoBehaviour
         before[0].GetComponent<RectTransform>().anchoredPosition =  new Vector3(51, -49,0);
 
     }
+    public void Toilet()
+    {
+        before[0].sprite = GameManager.Instance.changeImage[0];
+        before[0].GetComponent<RectTransform>().sizeDelta = new Vector2(700, 700);
+        before[0].GetComponent<RectTransform>().anchoredPosition = new Vector3(159, 40, 0);
+    }
+    public void Sink()
+    {
+        before[1].sprite = GameManager.Instance.changeImage[1];
+        before[1].GetComponent<RectTransform>().sizeDelta = new Vector2(700, 500);
+        before[1].GetComponent<RectTransform>().anchoredPosition = new Vector3(-66, 0, 0);
+    }
 }
